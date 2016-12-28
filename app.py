479f43d6fb93b01fcf6f8edeecd2d69803a012e1
moinudeen <moinu.95@gmail.com>
2016-12-28 23:09:09 +0530

    Added screenshot

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model, graph = init()
 
 def convertImage(imgData1):
 	imgstr = re.search(r'base64,(.*)',imgData1).group(1)
-	print(imgstr)
+	#print(imgstr)
 	with open('output.png','wb') as output:
 		output.write(imgstr.decode('base64'))
 	
@@ -33,16 +33,18 @@ def predict():
 	imgData = request.get_data()
 	#print(imgData)
 	convertImage(imgData)
-	#print (model)
+	print "debug"
 	x = imread('output.png',mode='L')
 	x = np.invert(x)
 	x = imresize(x,(28,28))
 	#imshow(x)
 	x = x.reshape(1,28,28,1)
+	print "debug2"
 	with graph.as_default():
 		out = model.predict(x)
 		print(out)
 		print(np.argmax(out,axis=1))
+		print "debug3"
 		response = np.array_str(np.argmax(out,axis=1))
 		return response	
 	
